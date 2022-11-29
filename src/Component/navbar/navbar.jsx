249e51fb83438180dbe138ab6d845470d0b81388
dhiamlayah <113603828+dhiamlayah@users.noreply.github.com>
2022-11-29 17:05:18 +0000

    first commit

--- src/Component/navbar/navbar.jsx
+++ src/Component/navbar/navbar.jsx
@@ -1,31 +1,55 @@
-import React, { Component } from 'react';
-import './navbar.css';
+import React, { Component } from "react";
+import "./navbar.css";
+
 // import logo from '../assets/logo.svg'
 class Navbar extends Component {
   render() {
     return (
-      <div className='project__navbar'>
-        <div className='project__navbar--links'>
-          <div className='project__navbar--links_logo'>
-             <img src="images/Present.png" alt="a" /> 
+      <div className="project__navbar">
+        <div className="project__navbar--links">
+         
+
+          <div className="project__navbar--links_logo">
+            <div className="project__navbar--links_img">
+              <img src="images/Present.png" alt="a" />
+            </div>
+            <div className="project__navbar--links_Name">
+              <div className="part1"><h1>p</h1></div>
+              <div className="part2">
+                <h2> resent</h2>
+                <h3>Your career</h3>
+              </div>
+            </div>
           </div>
-          <div className='project__navbar--links_Name'>
-              <h1>Present</h1>
-             <h3>your career</h3>
-          </div>
-          <div className='project__navbar--links_container'>
-            <p><a href='#home'>Home</a></p>
-            <p><a href='#wpro'>Models</a></p>
-            <p><a href='#possibility'>Espace entreprise</a></p>
-            <p><a href='#features'>FAQ</a></p>
+
+
+
+
+
+          <div className="project__navbar--links_container">
+            <p>
+              <a href="#home">Home</a>
+            </p>
+            <p>
+              <a href="#wpro">Models</a>
+            </p>
+            <p>
+              <a href="#possibility">Espace entreprise</a>
+            </p>
+            <p>
+              <a href="#features">FAQ</a>
+            </p>
           </div>
         </div>
-        <div className='project__navbar--sign'>
+        <div className="project__navbar--sign">
           <p>sign in :</p>
-          <button type='button'><a href='#signup' className='aa'>sign up</a></button>
+          <button type="button">
+            <a href="#signup" className="aa">
+              sign up
+            </a>
+          </button>
         </div>
-        <div className='project__navbar--menu'>
-        </div>
+        <div className="project__navbar--menu"></div>
       </div>
     );
   }
--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -5,18 +5,51 @@ class Footer extends Component {
     render() {
         return (
             <div className='project__footer'>
-                <div className='project__footer--links'>
-                    <div className='project__footer--links_container'>
-                        <p><a href="#features">FAQ</a></p>
-                        <p><a href="#contact">CONTACT</a></p>
+                 <div className='ressource'>
+                    <h3>Ressources :</h3><br />
+                     <a href="">Conseils</a><br />
+                     <a href="">exemples</a>
+                    
+                  </div>
+                 <div className='service-client'>
+                    <h3>service client :</h3> <br />
+                    <ul  type="1">
+                        <a href="">FAQ</a><br />
+                        <a href="">Contact</a><br />
+                        <a href="">À propos</a>
+                    </ul>
+                 </div>
+                 <div className='aboutus'>
+                     <div className='titel'><h3>About us :</h3></div>
+                    <div className='socialmedia'>
+                    <details>
+                        <summary><img src="images/socialmedia/face.png" alt="" /></summary>
+                        <a href="">dhia</a><br />
+                        <a href="">haithem</a> <br />
+                        <a href="">med aziz</a> <br />
+                        <a href="">rayen</a>
+                    </details>
+                    <details>
+                        <summary><img src="images/socialmedia/email.png" alt="" /></summary>
+                        <a href="">dhia</a><br />
+                        <a href="">haithem</a> <br />
+                        <a href="">med aziz</a> <br />
+                        <a href="">rayen</a>
+                    </details><details>
+                        <summary><img src="images/socialmedia/linkedin.png" alt="" /></summary>
+                        <a href="">dhia</a><br />
+                        <a href="">haithem</a> <br />
+                        <a href="">med aziz</a> <br />
+                        <a href="">rayen</a>
+                    </details><details>
+                        <summary><img src="images/socialmedia/insta.png" alt="" /></summary>
+                        <a href="">dhia</a><br />
+                        <a href="">haithem</a> <br />
+                        <a href="">med aziz</a> <br />
+                        <a href="">rayen</a>
+                    </details>
                     </div>
-                    <div className='project__footer--links_logo'>
-                        <a href="https://www.facebook.com/monta.khaled.96/"><img src='images/linkdin.png' alt='img0' /></a>
-                        <a href="https://www.facebook.com/monta.khaled.96/"><img src='images/facebook.png' alt='img0' /></a>
-                        <a href="https://www.facebook.com/monta.khaled.96/"><img src='images/instagram.png' alt="aa" /></a>
-                    </div>
-                </div>
-
+                 </div> <br />
             </div>
         );
     }
